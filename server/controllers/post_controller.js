33898e28a768a54e85d4a78037085b1da4e793e5
ridhaadev <ridhaadev@gmail.com>
2022-06-19 21:36:05 +0200

    Added liking and unliking a post

--- server/controllers/post_controller.js
+++ server/controllers/post_controller.js
@@ -2,63 +2,81 @@ import PostModel from "../models/post_model.js";
 import mongoose from "mongoose";
 
 // create new post
-export const create_post = async(req, res) => {
-    const new_post = new PostModel(req.body)
+export const create_post = async (req, res) => {
+  const new_post = new PostModel(req.body);
 
-    try {
-        await new_post.save()
-        res.status(200).json("Post created")
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+  try {
+    await new_post.save();
+    res.status(200).json("Post created");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 // get a post
-export const get_post = async(req, res) => {
-    const id = req.params.id
+export const get_post = async (req, res) => {
+  const id = req.params.id;
 
-    try {
-        const post = await PostModel.findById(id)
+  try {
+    const post = await PostModel.findById(id);
 
-        res.status(200).json(post)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-}
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 // update a post
-export const update_post = async(req, res) => {
-    const post_id = req.params.id
-    const { user_id } = req.body
+export const update_post = async (req, res) => {
+  const post_id = req.params.id;
+  const { user_id } = req.body;
 
-    try {
-        const post = await PostModel.findById(post_id)
-        // Users can only update their own posts
-        if (post.user_id === user_id) {
-            await post.updateOne({ $set: req.body })
-            res.status(200).json("Post updated")
-        } else {
-            res.status(403).json("Action forbidden")
-        }
-    } catch (error) {
-        res.status(500).json(error)
+  try {
+    const post = await PostModel.findById(post_id);
+    // Users can only update their own posts
+    if (post.user_id === user_id) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post updated");
+    } else {
+      res.status(403).json("Action forbidden");
     }
-}
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 // delete a post
-export const delete_post = async(req, res) => {
-    const id = req.params.id
-    const { user_id } = req.body
+export const delete_post = async (req, res) => {
+  const id = req.params.id;
+  const { user_id } = req.body;
 
-    try {
-        const post = await PostModel.findById(id)
-        if (post.user_id === user_id) {
-            await post.deleteOne()
-            res.status(200).json("Post sucessfully deleted")
-        } else {
-            res.status(403).json("Action forbidden")
-        }
-    } catch (error) {
-        
+  try {
+    const post = await PostModel.findById(id);
+    if (post.user_id === user_id) {
+      await post.deleteOne();
+      res.status(200).json("Post sucessfully deleted");
+    } else {
+      res.status(403).json("Action forbidden");
     }
-}
+  } catch (error) {}
+};
+
+// like and dislike a post
+export const like_post = async (req, res) => {
+  const id = req.params.id;
+  const { user_id } = req.body;
+
+  try {
+    const post = await PostModel.findById(id);
+    // if the user didn't like the post already
+    if (!post.likes.includes(user_id)) {
+      await post.updateOne({ $push: { likes: user_id } });
+      res.status(200).json("Post liked");
+    } else {
+      await post.updateOne({ $pull: { likes: user_id } });
+      res.status(200).json("Post unliked");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- server/routes/post_routes.js
+++ server/routes/post_routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { create_post, delete_post, get_post, update_post } from "../controllers/post_controller.js";
+import { create_post, delete_post, get_post, like_post, update_post } from "../controllers/post_controller.js";
 const router = express.Router()
 
 router.post('/', create_post)
 router.get('/:id', get_post)
 router.put('/:id', update_post)
 router.delete('/:id', delete_post)
+router.put('/:id/like', like_post)
 
 export default router
