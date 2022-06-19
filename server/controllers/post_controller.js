827c3ab535e6aa9d6cb81c80a0dda8795a32be05
ridhaadev <ridhaadev@gmail.com>
2022-06-19 22:09:53 +0200

    FInished off first timeline posts attempt, api tested

--- server/controllers/post_controller.js
+++ server/controllers/post_controller.js
@@ -1,5 +1,6 @@
 import PostModel from "../models/post_model.js";
 import mongoose from "mongoose";
+import UserModel from "../models/user_model.js";
 
 // create new post
 export const create_post = async (req, res) => {
@@ -76,6 +77,48 @@ export const like_post = async (req, res) => {
       await post.updateOne({ $pull: { likes: user_id } });
       res.status(200).json("Post unliked");
     }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// get timeline post
+export const get_timeline_posts = async (req, res) => {
+  const user_id = req.params.id;
+
+  try {
+    const current_user_posts = await PostModel.find({ user_id: user_id });
+    const following_posts = await UserModel.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(user_id),
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "followers",
+          foreignField: "user_id",
+          as: "following_posts",
+        },
+      },
+      {
+        $project: {
+          following_posts: 1,
+          _id: 0,
+        },
+      },
+    ]);
+
+    // timeline posts current consists of user posts plus
+    // following posts, concatted and sorted latest first
+    const timeline_posts = current_user_posts
+      .concat(...following_posts[0].following_posts)
+      .sort((a, b) => {
+        return b.createdAt - a.createdAt;
+      });
+
+    res.status(200).json(timeline_posts);
   } catch (error) {
     res.status(500).json(error);
   }
--- server/routes/post_routes.js
+++ server/routes/post_routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { create_post, delete_post, get_post, like_post, update_post } from "../controllers/post_controller.js";
+import { create_post, delete_post, get_post, get_timeline_posts, like_post, update_post } from "../controllers/post_controller.js";
 const router = express.Router()
 
 router.post('/', create_post)
@@ -7,5 +7,6 @@ router.get('/:id', get_post)
 router.put('/:id', update_post)
 router.delete('/:id', delete_post)
 router.put('/:id/like', like_post)
+router.get("/:id/timeline", get_timeline_posts)
 
 export default router
